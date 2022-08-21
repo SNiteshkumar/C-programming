b4220c5132f96b3fd78958befdaa96fb78f25a06
SNiteshkumar <111690374+SNiteshkumar@users.noreply.github.com>
2022-08-21 21:06:32 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,23 @@
+# Write program to display the contents of a file on screen
+
+#include <stdio.h>	
+void main() 
+{
+  FILE *fp;
+  int c;
+  fp = fopen("file.txt", "r");
+  if(fp == NULL)
+  {
+    printf("File does not exist, please check file location!\n");
+  }
+  else
+  {
+    c = getc(fp) ;
+    while(c != EOF)
+    {
+      putchar(c);
+      c = getc(fp);
+    }
+  }
+  fclose(fp);
+}
