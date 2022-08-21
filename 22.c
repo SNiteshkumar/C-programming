342d0ad19ee83e7f7c9bce450b4be03cfd3b0d03
SNiteshkumar <111690374+SNiteshkumar@users.noreply.github.com>
2022-08-21 20:54:38 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,25 @@
+# Write a program of reading and writing two dimensional array using pointer
+
+#include <stdio.h>
+void main() 
+{
+  int a[10][10];
+  int i, j, row = 3, col = 3;
+  printf("Enter array elements: ");
+  for(i = 0; i < row; i++)
+  {
+    for(j = 0; j < col; j++)
+    {
+      scanf("%d", &a[i][j]);
+    }
+  }
+  printf("Output of array elements\n");
+  for(i = 0; i < row; i++)
+  {
+    for(j = 0; j < col; j++)
+    {
+      printf("%d  ", *(*(a + i) + j));
+    }
+    printf("\n");
+  }
+}
