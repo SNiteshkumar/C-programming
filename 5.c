51cd8d17dd92c856393197157a438836f6c67b91
SNiteshkumar <111690374+SNiteshkumar@users.noreply.github.com>
2022-08-21 20:33:03 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+# program to check even number
+
+#include <stdio.h>
+main() 
+{
+  int a;
+  printf("Enter the value of a: ");
+  scanf("%d",&a);
+  if (a%2 == 0) 
+  {
+    printf("%d is even number.", a);
+  }
+} 
