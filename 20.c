788abaaf8bb9eac5efeff97b5e18462608399e5b
SNiteshkumar <111690374+SNiteshkumar@users.noreply.github.com>
2022-08-21 20:50:59 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,22 @@
+# Program to find factorial using recursion in C
+
+#include <stdio.h>
+int factorial(int);
+void main() 
+{
+  int n;
+  printf("Enter any number: ");
+  scanf("%d", &n);
+  printf("Factorial of %d is %d", n, factorial(n));
+}
+int factorial(int n) 
+{
+  if(n == 0)
+  {
+    return 1;
+  }
+  else 
+  {
+    return n * factorial(n - 1);
+  }
+}
