66c226cbaa87c108692a7417c91c9303140f067f
SNiteshkumar <111690374+SNiteshkumar@users.noreply.github.com>
2022-08-21 20:30:12 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,10 @@
+# Program to compare two numbers using Conditional operator
+
+#include <stdio.h>
+main() 
+{
+  int a, b;
+  printf("Please enter values for a and b: ");
+  scanf("%d%d",&a,&b);
+  (a>b)?printf("%d is greater.", a):printf("%d is greater.", b);
+} 
